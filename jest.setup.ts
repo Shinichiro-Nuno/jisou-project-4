10d39520b3f068c25ec48ebcf98537d7ebd99a67
Shinichiro-Nuno <nuno1216shin@gmail.com>
2024-12-27 21:12:59 +0900

    Chakra UI V3導入テスト2

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,5 @@
 import "@testing-library/jest-dom";
 
 require("dotenv").config();
+
+global.structuredClone = (val) => JSON.parse(JSON.stringify(val));
--- src/__tests__/AppComponent.spec.tsx
+++ src/__tests__/AppComponent.spec.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "../App";
 import { render, screen } from "@testing-library/react";
 
 describe("title", () => {
   it("should render title", () => {
-    render(<App />);
+    render(
+      <ChakraProvider value={defaultSystem}>
+        <App />
+      </ChakraProvider>
+    );
     expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
 });

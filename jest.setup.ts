e1e67275a3193840dae4ca4f3e74fb9f5db6815f
Shinichiro-Nuno <nuno1216shin@gmail.com>
2024-12-29 21:57:49 +0900

    TextEncoderの追加

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,5 @@ import "@testing-library/jest-dom";
 require("dotenv").config();
 
 global.structuredClone = (val) => JSON.parse(JSON.stringify(val));
+
+global.TextEncoder = require("util").TextEncoder;
--- src/__tests__/AppComponent.spec.tsx
+++ src/__tests__/AppComponent.spec.tsx
@@ -1,13 +1,16 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router";
 
 describe("title", () => {
   it("should render title", () => {
     render(
-      <ChakraProvider value={defaultSystem}>
-        <App />
-      </ChakraProvider>
+      <BrowserRouter>
+        <ChakraProvider value={defaultSystem}>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
     );
     expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
